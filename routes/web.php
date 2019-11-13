11795d837603426443bbd22ebacb8f270971efad
ChristianAlbaladejo <57177978+ChristianAlbaladejo@users.noreply.github.com>
2019-11-13 13:44:53 +0100

    Ejercicio listaCompra

--- routes/web.php
+++ routes/web.php
@@ -16,25 +16,25 @@ Route::get('/', function () {
 });
 
 Route::get('/login', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::get('/logout', function () {
-    return view('welcome');
+    return "logout";
 });
 
 Route::get('/productos', function () {
-    return view('welcome');
+    return view('productos.index');
 });
 
 Route::get('/productos/show/{id}', function ($id) {
-    return view('welcome');
+    return view('productos.show', array('id' => $id));
 });
 
 Route::get('/productos/create', function () {
-    return view('welcome');
+    return view('productos.create');
 });
 
 Route::get('/productos/edit/{id}', function ($id) {
-    return view('welcome');
+    return view('productos.edit', array('id' => $id));
 });
